7b07a675cdf004ba3040175ab26c8548c30e7958
unknown <taiwokolapo533@gmail.com>
2026-04-17 13:22:55 +0100

    working on product

--- src/page/ProductListing.jsx
+++ src/page/ProductListing.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function ProductListing() {
   return (
     <div>
-      
+      <div className=''>
+        {/* title */}
+        <p className='text-[12px] text-[#0F766E] font-semibold'>EDITORIAL EDITION</p>
+        <h1 className='text-[48px] font-semibold font-jakarta text-[#00535B]'>Elevated Essentials.</h1>
+        <p className='w-5/10 text-[#3E494A] text-[16px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur molestiae asperiores est quidem! Placeat, nobis dignissimos? Facilis hic temporibus quasi!</p>
+      </div>
+
+      {/* product listing */}
+      <div>
+
+      </div>
     </div>
   )
 }
